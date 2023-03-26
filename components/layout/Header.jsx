7cf078e45fed53b457f0f42293c68bec2bf2d295
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2023-03-26 23:15:48 +0300

    End of 3.2 (11)

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,7 +1,11 @@
-import Logo from "../ui/Logo"
+import { useState } from "react"
 import {FaUserAlt, FaShoppingCart, FaSearch} from 'react-icons/fa'
+import OutsideClickHandler from "react-outside-click-handler"
+import Logo from "../ui/Logo"
+import Title from "../ui/Title"
 
 const Header = () => {
+    const [isSearchModal, setIsSearchModal] = useState(false)
   return (
     <div className="h-[5.5rem] bg-secondary">
         <div className="container mx-auto text-white flex justify-between items-center h-full">
@@ -23,20 +27,27 @@ const Header = () => {
                 </ul>
             </nav>
             <div className="flex gap-x-4 items-center">
-                <a className="link-item " href="">
+                <a className="link-item" href="">
                     <FaUserAlt/>
                 </a>
-                <a className="link-item " href="">
+                <a className="link-item" href="">
                     <FaShoppingCart />
                 </a>
-                <a className="link-item " href="">
+                <button className="link-item" onClick={() => setIsSearchModal(!isSearchModal)}>
                     <FaSearch />
-                </a>
-                <a className="link-item " href="">
+                </button>
+                <a className="link-item" href="">
                     <button className="btn-primary">Order Online</button>
                 </a>
             </div>
         </div>
+        {isSearchModal && (
+            <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
+                <div className="">
+                    <Title className="font-dancing text-2xl">Search</Title>
+                </div>
+            </OutsideClickHandler>
+        )}
     </div>
   )
 }
---
+++ components/ui/Title.jsx
@@ -0,0 +1,7 @@
+const Title = ({children, className}) => {
+  return (
+    <div className={className}>{children}</div>
+  )
+}
+
+export default Title
