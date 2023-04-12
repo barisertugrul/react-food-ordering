657597e003c6e3da86bba3c2eeb61ee43dafe628
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2023-04-13 01:07:45 +0300

    End of 5.9 (24)

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import Input from './form/Input'
 import Title from './ui/Title'
+import { Formik, useFormik } from 'formik';
 
 const Reservation = () => {
+
+    const onSubmit = async (values, actions) => {
+        await new Promise((resolve) => setTimeout(resolve, 400))
+        actions.resetForm()
+    }
+    const {values, handleSubmit, handleChange} = useFormik({
+        initialValues: {
+            fullName: "",
+            phoneNumber: "",
+            email: "",
+            persons: "",
+            date: ""
+        },
+        onSubmit,
+    })
+
     const inputs = [
         {
             id: 1,
             name: "fullName",
             type: "text",
             placeholder: "Your Full Name",
+            value: values.fullName,
             isrequired:true
         },
         {
@@ -16,6 +34,7 @@ const Reservation = () => {
             name: "phoneNumber",
             type: "text",
             placeholder: "Your Phone Number",
+            value: values.phoneNumber,
             isrequired:true
         },
         {
@@ -23,6 +42,7 @@ const Reservation = () => {
             name: "email",
             type: "email",
             placeholder: "Your E-mail Address",
+            value: values.email,
             isrequired:true
         },
         {
@@ -30,28 +50,29 @@ const Reservation = () => {
             name: "persons",
             type: "number",
             placeholder: "How Many Persons?",
+            value: values.persons,
             isrequired:true
         },
         {
             id: 5,
             name: "date",
             type: "datetime-local",
-            placeholder: "Date",
+            value: values.date,
             isrequired:true
         },
     ]
   return (
-    <div className='container mx-auto py-12 '>
+    <div className='container mx-auto py-12 '  >
         <Title className="text-[40px] mb-10">Book A Table</Title>
         <div className='flex justify-between flex-wrap-reverse gap-10'>
-            <div className='md:flex-1 w-full'>
+            <form className='md:flex-1 w-full'  onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-y-3'>
                     {inputs.map((input) => (
-                        <Input key={input.id} {...input} />
+                        <Input key={input.id} {...input} onChange={handleChange} />
                     ))}
                 </div>
-                <button className="btn-primary mt-5 md:mb-1 mb-5">BOOK NOW</button>
-            </div>
+                <button type='submit' className="btn-primary mt-5 md:mb-1 mb-5">BOOK NOW</button>
+            </form>
             <div className='md:flex-1 w-full'>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25007.885492155172!2d27.120808264317812!3d38.41869566264328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14bbd8e3fa1744e7%3A0xfc15e392fd3613c8!2zS2VtZXJhbHTEsSDDh2FyxZ_EsXPEsQ!5e0!3m2!1str!2str!4v1681069147666!5m2!1str!2str"
